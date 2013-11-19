2f05802c784c79b95fab1b986d0847e054f6bd92
luckymonkey <luckymonkey@almost-there.org>
2013-11-18 19:16:19 -0500

    Added Youtube videos

--- index.php
+++ index.php
@@ -13,18 +13,23 @@ else { include 'import.php'; } ?>
 <div class='fl youTube cf' >
 	<iframe src="//www.youtube.com/embed/<? 
 	$yts = array(
-	"OKxaPkIx4WY", /* Oxygen - Moguai feat. Fiora */
-	"d1wGFgoocKk", /* Schoolboy - Project No-Autotune */
-	"v6_qfgHak0Q", /* Hellberg, Teqq & Taylr Renee - Air (Monstercat Remix) */
-	"n9nntf5_JXc", /* MSD - I Feel You (Bluescreens Remix) */
-	"lG5aSZBAuPs" /* XXYYXX - About You */
-	"2qN5ewi44N8" /* Motionless in WHite - AMERICA */
-	"uxelXPg961M", /* Mastodon - Divinations */
-	"FaJytyOISmE", /* Pasafire - Rubber Bands */
-	"VuwwvokPaQ", /* The Airplane Boys - Beau Monde */
-	"3LqsWSvnjGs", /* Deadmau5 - asdfghjkl */
-	"Z0NefmRM81Y", /* Sunchase feat. Yana Kay - Remember Me */
-	"Q1KAjQRdTUE");/* Fatboy Slim - Right Here Right Now (Trumpdisco Remix)*/
+	"OKxaPkIx4WY", 		/* Oxygen - Moguai feat. Fiora */
+	"d1wGFgoocKk",		/* Schoolboy - Project No-Autotune */
+	"v6_qfgHak0Q", 		/* Hellberg, Teqq & Taylr Renee - Air (Monstercat Remix) */
+	"n9nntf5_JXc", 			/* MSD - I Feel You (Bluescreens Remix) */
+	"lG5aSZBAuPs" 		/* XXYYXX - About You */
+	"2qN5ewi44N8" 		/* Motionless in White - AMERICA */
+	"uxelXPg961M", 		/* Mastodon - Divinations */
+	"FaJytyOISmE", 		/* Pasafire - Rubber Bands */
+	"k8gpHUIZaVQ",		/* Boreta - Honey Heart */
+	"8wCgxkQj_Qo", 		/* Pretty Lights - I Know The Truth */
+	"f9EM8T5K6d8",		/* Modstep - Another Day (Xcore Remix) */
+	"ShU3_-Y6SH0",		/* Celldweller - Goodbye 2012 Revision */
+	"8mYd2X_9rrs",		/* Pendulum - Hold your Colour */
+	"VuwwvokPaQ", 		/* The Airplane Boys - Beau Monde */
+	"3LqsWSvnjGs", 		/* Deadmau5 - asdfghjkl */
+	"Z0NefmRM81Y", 	/* Sunchase feat. Yana Kay - Remember Me */
+	"Q1KAjQRdTUE");	/* Fatboy Slim - Right Here Right Now (Trumpdisco Remix) */
 	$rYt = rand(0,count($yts)-1);
 	$yt= $yts[$rYt];
 	echo $yt;
