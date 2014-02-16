9296447368d574c9548ef214be6d7e41edc65973
luckymonkey <chredden4@gmail.com>
2014-02-16 11:03:46 -0500

    Added ALOT of music, alphabetized the list

--- squares.php
+++ squares.php
@@ -74,48 +74,118 @@ function spawnSquare($=)
 		<div class='sqYt'>
 		<? echo "<iframe src='//www.youtube.com/embed/";
 		$yts = array(
-			"OKxaPkIx4WY",	// Oxygen - Moguai feat. Fiora
-			"d1wGFgoocKk",	// Schoolboy - Project No-Autotune
-			"n9nntf5_JXc",	// MSD - I Feel You (Bluescreens Remix)
-			"lG5aSZBAuPs",	// XXYYXX - About You
-			"lM7XgAmcXp8",	// InnerPartySystem - American Trash
-			"2qN5ewi44N8",	// Motionless in White - AMERICA
-			"uxelXPg961M",	// Mastodon - Divinations
-			"9QZ2ZvueDbA",	// Neonlight - Computer Music
-			"uQu71l1WQ3g",	// Becky - Less than Three
-			"DAQRNnNXb6w",	// AWOLNATION - Sail (Borgore Remix)
-			"9nVvojfQVBY",	// Mudvayne - Death Blooms
-			"reHGIYHAR2Q",	// Borgore - Nympho
-			"0x6PhH6v0Ls",	// Gemini - Graduation
-			"t5-QSLt56JY",	// SOFI - Bring out the Devil
-			"0NS53Ydzq6o",	// Noisia - Stigma (NeoSignal Remix)
-			"Z6hL6fkJ1_k",	// Infected Mushroom - Becoming Insane
-			"tVL1u1eLX3I",	// The Prodigy - Baby's got a Temper
-			"EH9meoWmAOM",	// Coldplay - Talk
-			"etdlQtRP9zw",	// Deadmau5 - My Pet Coelacanth
-			"kbpqZT_56Ns",	// Foo Fighters - Rope
+			"xVYp7khUDbw",	// Air - Mer du Japon The Teenagers Remix
+			"CDOIG8HKUZA",	// The Airplane Boys - Beau Monde
+			"-LHrHo-WNww",	// Alex Gaudino - Destination Unknown Feat. Crystal Waters
 			"fxvGHQHiY70",	// Animal Collective - Peacebone
-			"xAEhBQZ-k7U",	// InnerPartySystem - And Together
-			"_NBrrCn5_zo",	// Pendulum - Comprachicos
-			"5AA1rfKkWkI",	// Ellie Goulding - Explosions (Gemini Remix)
+			"nm_rtM2HgI4",	// ASAP Rocky - Brand New Guy feat. ScHoolboy Q
+			"qLrnkK2YEcE",	// The Avalanches - Frontier Psychiatrist
+			"DAQRNnNXb6w",	// AWOLNATION - Sail (Borgore Remix)
+			"uQu71l1WQ3g",	// Becky - Less than Three
+			"SREZ-ggSDjM",	// The Birthday Massacre - Looking Glass
 			"EjAoBKagWQA",	// Bjork - All is full of love
-			"u7K72X4eo_s",	// Massive Attack - Teardrop
-			"YXdOAUKCc0k",	// Red Hot Chili Peppers - By The Way
-			"QRg-aDBaVdc",	// Gorillaz - Dare
-			"FaJytyOISmE",	// Pasafire - Rubber Bands
-			"xsV8TrF4gN0",	// Not In Love - Crystal Castles
-			"C-9v68o5Gfs",	// Voicans - Loyal Friend
+			"NhheiPTdZCw",	// Blockhead - The Music Scene
+			"xat1GVnl8-k",	// Bloodhound Gang - The Bad Touch
+			"yhIPGuGUPl4",	// The Bloody Beetroots - Out of Sight
 			"k8gpHUIZaVQ",	// Boreta - Honey Heart
-			"HEkLU2AiJCs",	// Shiny Toy Guns - Stripped
-			"8wCgxkQj_Qo",	// Pretty Lights - I Know The Truth
-			"f9EM8T5K6d8",	// Modstep - Another Day (Xcore Remix)
-			"PQ1PU1XOQIk",	// Styles of Beyond - Bleach
+			"reHGIYHAR2Q",	// Borgore - Nympho
+			"Mzybwwf2HoQ",	// Britney Spears - I'm A Slave 4 U
+			"EH9meoWmAOM",	// Coldplay - Talk
 			"ShU3_-Y6SH0",	// Celldweller - Goodbye 2012 Revision
-			"8mYd2X_9rrs",	// Pendulum - Hold your Colour
-			"VuwwvokPaQ",	// The Airplane Boys - Beau Monde
+			"I744rclQLfQ",	// Celldweller - Unshakeable
+			"e2QKlmMT8II",	// 2Chains - I'm Different
+			"Xu3FTEmN-eg",	// The Chemical Brothers - Galvanize
+			"xsV8TrF4gN0",	// Crystal Castles - Not in Love
+			"A12-KN5UijA",	// The Crystal Method - Name of the Game
+			"H6BEkPzstJQ",	// The Crystal Method - Born to Slow
+			"lwU0SlMu07E",	// CygnosiC - As We Approach The End
+			"xcyuKUtgyZ8",	// David Bowie - Space Oddity
 			"3LqsWSvnjGs",	// Deadmau5 - asdfghjkl
-			"Z0NefmRM81Y",	// Sunchase feat. Yana Kay - Remember Me
-			"Q1KAjQRdTUE"	// Fatboy Slim - Right Here Right Now (Trumpdisco Remix)
+			"MSBSlyMZCfA",	// Deadmau5 - Infra Super Turbo Pig Cart Racer	
+			"etdlQtRP9zw",	// Deadmau5 - My Pet Coelacanth
+			"m0AKJMGxwpE",	// Depeche Mode - Enjoy the Silence
+			"_f0zHgs0GpI",	// Depeche Mode - Useless
+			"3m3yWUqC8ZM",	// Disturbed - Decadence
+			"5AA1rfKkWkI",	// Ellie Goulding - Explosions (Gemini Remix)
+			"hAuo8IOFNuE",	// Eminem - 3AM
+			"YVkUvmDQ3HY",	// Eminem - Without Me
+			"DG7P6NX8yyE",	// Faderhead - Dirtygrrrls/Dirtybois
+			"Q1KAjQRdTUE",	// Fatboy Slim - Right Here Right Now (Trumpdisco Remix)
+			"-5nUg3EvUS8",	// Figure - Must Distroy
+			"iWIADZKU9dw",	// Flyleaf - I'm So Sick
+			"kbpqZT_56Ns",	// Foo Fighters - Rope
+			"FMBDrB_Scqs",	// Franz Ferdinand - Do You Want To (Metronomy remix)
+			"0x6PhH6v0Ls",	// Gemini - Graduation
+			"YeawPUpTHJA",	// Goldfrapp - Strict Machine
+			"nFF8bubMc40",	// Goldfrapp - White Horse
+			"nhPaWIeULKk",	// Gorillaz - Stylo
+			"QRg-aDBaVdc",	// Gorillaz - Dare
+			"f5qICl3Fr3w",	// Gwen Stefani - What You Waiting For?
+			"ON-7v4qnHP8",	// Harry Enfield: loadsamoney
+			"Z6hL6fkJ1_k",	// Infected Mushroom - Becoming Insane
+			"6-uqXH-oPLs",	// Infected Mushroom - Killing Time (Remix)
+			"lM7XgAmcXp8",	// InnerPartySystem - American Trash
+			"xAEhBQZ-k7U",	// InnerPartySystem - And Together
+			"C9dEKatWIBw",	// iwillneverbethesame - Tornadoes
+			"pwgMMtgSTVE",	// iwrestledabearonce - You know that ain't them dogs real voices
+			"f4hbJ5zh-SQ",	// Jarren Benton - Skitzo
+			"WANNqr-vcx0",	// Jefferson Airplane - White Rabbit
+			"o22eIJDtKho",	// Johnny Cash - Hurt
+			"e7kJRGPgvRQ",	// KORPIKLAANI - Vodka
+			"VTT6picaCoQ",	// Lacuna Coil - Trip The Darkness
+			"USXHxgWoS9g",	// Lacuna Coil - End of Time
+			"8VqJGRHuKUE",	// The Left Rights - WHITE
+			"0GLoHifu6aM",	// The Left Rights - I'm on Crack
+			"mrukUsEjoVI",	// The Luna Sequence - Surfacing
+			"AIIovpUQiro",	// Massive Attack - Live with Me
+			"u7K72X4eo_s",	// Massive Attack - Teardrop
+			"uxelXPg961M",	// Mastodon - Divinations
+			"5SWfOLg-LeY",	// Mayhem - 6 Little Eggs (Furries in a blender remix)
+			"B0AX81gv5aM",	// Mindless Self Indulgance - Shut Me Up
+			"X4CrrP5G3v0",	// Mindless Self Indulgance - Stupid MF
+			"JxDdN-puo3g",	// Mobb Deep - Survival of the Fittest
+			"f9EM8T5K6d8",	// Modstep - Another Day (Xcore Remix)
+			"OKxaPkIx4WY",	// Moguai - Oxygen feat. Fiora
+			"2qN5ewi44N8",	// Motionless in White - AMERICA
+			"n9nntf5_JXc",	// MSD - I Feel You (Bluescreens Remix)
+			"9nVvojfQVBY",	// Mudvayne - Death Blooms
+			"B3JHH14xEoQ",	// Muzzy - Endgame
+			"9QZ2ZvueDbA",	// Neonlight - Computer Music
+			"0NS53Ydzq6o",	// Noisia - Stigma (NeoSignal Remix)
+			"j34juXrJWqw",	// Ozzy Osborne - Miricle Man
+			"FaJytyOISmE",	// Pasafire - Rubber Bands
+			"MSv3Oez4O-4",	// Paul Oakenfold - Ready, Steady, Go
+			"kHadNTiftrg",	// Paul Van Dyk - Let Go
+			"aDaOgu2CQtI",	// Pearl Jam - Do the Evolution
+			"_NBrrCn5_zo",	// Pendulum - Comprachicos
+			"8mYd2X_9rrs",	// Pendulum - Hold your Colour
+			"YkADj0TPrJA",	// Phil Collins - In The Air Tonight
+			"LWNteSeRHec",	// Pretty Lights - Hot like Dimes
+			"8wCgxkQj_Qo",	// Pretty Lights - I Know The Truth
+			"tVL1u1eLX3I",	// The Prodigy - Baby's got a Temper
+			"0NvnWJU5Ak8",	// Rammstien - Ich tu dir weh
+			"LOwHX8PmnMU",	// Rammstien - Ich Will
+			"YXdOAUKCc0k",	// Red Hot Chili Peppers - By The Way
+			"PlJBUaozeM8",	// 30 Seconds to Mars - Edge of the Earth
+			"d1wGFgoocKk",	// Schoolboy - Project No-Autotune
+			"8aSHmH9XCNU",	// SKazi - My Way
+			"yp3-jmRXIww",	// Shallow Gravy - Jacket
+			"HEkLU2AiJCs",	// Shiny Toy Guns - Stripped
+			"v_09wFxoaeQ",	// Slipknot - Before I forget
+			"9rQz0xGaTCE",	// Slipknot - Left Behind
+			"t5-QSLt56JY",	// SOFI - Bring out the Devil
+			"312Sb-2PovA",	// Suicide Silence - You Only Live Once
+			"Z0NefmRM81Y",	// Sunchase - Remember Me feat. Yana Kay
+			"PQ1PU1XOQIk",	// Styles of Beyond - Bleach
+			"sYOlzMOFU2w",	// Sybreed - The Line of Least Resistance
+			"8mGBaXPlri8",	// t.A.T.u. - All The Things She Said
+			"98W9QuMq-2k",	// U2 - Vertigo
+			"BSsMGNwYDdg",	// Unkle - Burn My Shadow
+			"VLVtIbfQtEw",	// Unkle - Inside
+			"1VH2mrAzNjI",	// Varien - Morphine
+			"C-9v68o5Gfs",	// Voicans - Loyal Friend
+			"UePtoxDhJSw",	// Wiz Khalifa - Black And Yellow
+			"lG5aSZBAuPs"	// XXYYXX - About You
 			);
 			shuffle($yts);
 			$rYt = rand(0,count($yts)-1);
